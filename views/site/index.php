35a7e15a3ad228678e08cdacf026e0db18acc5c0
Jasurbek <jasurbekrasuljonovrj1225@gmail.com>
2022-01-27 15:51:38 +0500

    login page to'g'rilandi

--- views/site/index.php
+++ views/site/index.php
@@ -7,17 +7,9 @@ use yii\helpers\Html; ?>
 <?php
 $this->title = Yii::t('app','welcome');
 ?>
-
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="image text-center">Bu yerda rasm bo'ladi</div>
-            </div>
-        </div>
-        <div class="jumbotron">
-            <div class="image text-center">Bu yerda ham rasm bo'ladi</div>
-            <br>
-            <h3 class="text-orange"><?= mb_strtoupper(\Yii::t('app', 'Text about this system'))?></h3>
+    <div class="card">
+        <div class="card-body">
+            Index
         </div>
     </div>
 <?php
