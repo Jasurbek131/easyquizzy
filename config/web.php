04585846052964792d25122356c71fee9c120f59
fayzulloyakubov <developeryakubov@gmail.com>
2022-01-27 16:43:41 +0500

    custom GenerateGii

--- config/web.php
+++ config/web.php
@@ -71,7 +71,7 @@ if (YII_ENV_DEV) {
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
+        'class' => 'app\components\CustomGii\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
         //'allowedIPs' => ['127.0.0.1', '::1'],
     ];
