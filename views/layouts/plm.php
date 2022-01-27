43bd1c11a09e576098745cb487407a626a211189
fayzulloyakubov <developeryakubov@gmail.com>
2022-01-27 21:32:41 +0500

    hr position add menu

--- views/layouts/plm.php
+++ views/layouts/plm.php
@@ -251,6 +251,22 @@ AppAsset::register($this);
                                     ],
                                 ],
                             ],
+                            [
+                                'label' => Yii::t('app', 'Hr'),
+                                'url' => ['#'],
+                                'options' => ['class' => 'nav-item'],
+                                'template' => '<a href="{url}" class="{linkClass}"><i class="nav-icon fas fa-users"></i><p>{label}<i class="fas fa-angle-left right"></i></p></a>',
+                                'visible' => true,
+                                'items' => [
+                                    [
+                                        'label' => Yii::t('app', 'Hr Positions'),
+                                        'url' => '/hr/hr-positions/index',
+                                        'options' => ['class' => 'nav-item'],
+                                        'active' => $controller == 'hr-positions' && $action  == 'index',
+                                        'template' => '<a href="{url}" class="{linkClass}"><i class="fa fa-pager nav-icon"></i><p>{label}</p></a>',
+                                    ],
+                                ],
+                            ],
                         ],
 
                         'linkTemplate' => '<a href="{url}"><i class="fas fa-circle-o"></i> {label}</a>',
