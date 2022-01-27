98b0ea674aece5d7eef4fa77ebc172be442433c7
Jasurbek <jasurbekrasuljonovrj1225@gmail.com>
2022-01-27 16:50:53 +0500

    db.example ochirldi

---
+++ config/db.example.php
@@ -0,0 +1,39 @@
+<?php
+
+//return [
+//    'class' => 'yii\db\Connection',
+//    'dsn' => 'mysql:host=213.230.127.153:3307;dbname=prizma',
+//    'username' => 'prizma',
+//    'password' => 's@m02020',
+//    'charset' => 'utf8',
+//    'attributes' => [
+//        PDO::MYSQL_ATTR_INIT_COMMAND => "SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));",
+//    ],
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=textile_yii3',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+    'attributes' => [
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));",
+    ],
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
+
+//return [
+//    'class' => 'yii\db\Connection',
+//    'dsn' => 'pgsql:host=localhost;port=5432;dbname=test',
+//    'username' => 'user',
+//    'password' => '1997'
+//
+//    // Schema cache options (for production environment)
+//    //'enableSchemaCache' => true,
+//    //'schemaCacheDuration' => 60,
+//    //'schemaCache' => 'cache',
+//];
