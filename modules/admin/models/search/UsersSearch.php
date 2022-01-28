198a0f933d4122712adcaa18d9efaafe36f0b016
Jasurbek <jasurbekrasuljonovrj1225@gmail.com>
2022-01-28 16:23:41 +0500

    Users crud qo'shildi

---
+++ modules/admin/models/search/UsersSearch.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace app\modules\admin\models\search;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use app\models\Users;
+
+/**
+ * UsersSearch represents the model behind the search form of `app\models\Users`.
+ */
+class UsersSearch extends Users
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'status_id', 'created_at', 'updated_at', 'created_by', 'updated_by', 'hr_organisation_id'], 'integer'],
+            [['username', 'password', 'auth_key'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Users::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'status_id' => $this->status_id,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'created_by' => $this->created_by,
+            'updated_by' => $this->updated_by,
+            'hr_organisation_id' => $this->hr_organisation_id,
+        ]);
+
+        $query->andFilterWhere(['ilike', 'username', $this->username])
+            ->andFilterWhere(['ilike', 'password', $this->password])
+            ->andFilterWhere(['ilike', 'auth_key', $this->auth_key]);
+
+        return $dataProvider;
+    }
+}
---
+++ modules/admin/views/users/index.php
@@ -0,0 +1,99 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+/* @var $this yii\web\View */
+/* @var $searchModel app\modules\admin\models\search\UsersSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = Yii::t('app', 'Users');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="card users-index">
+    <?php if (Yii::$app->user->can('users/create')): ?>
+    <div class="card-header pull-right no-print">
+        <?= Html::a('<span class="fa fa-plus"></span>', ['create'],
+        ['class' => 'create-dialog btn btn-sm btn-success', 'id' => 'buttonAjax']) ?>
+    </div>
+    <?php endif; ?>
+    <div class="card-body">
+        <?php Pjax::begin(['id' => 'users_pjax']); ?>
+            <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+    
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterRowOptions' => ['class' => 'filters no-print'],
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'username',
+            'password',
+            'auth_key',
+            'status_id',
+            //'created_at',
+            //'updated_at',
+            //'created_by',
+            //'updated_by',
+            //'hr_organisation_id',
+
+                [
+                    'class' => 'yii\grid\ActionColumn',
+                    'template' => '{update}{view}{delete}',
+                    'contentOptions' => ['class' => 'no-print','style' => 'width:100px;'],
+                    'visibleButtons' => [
+                        'view' => Yii::$app->user->can('users/view'),
+                        'update' => function($model) {
+                            return Yii::$app->user->can('users/update'); // && $model->status < $model::STATUS_SAVED;
+                        },
+                        'delete' => function($model) {
+                            return Yii::$app->user->can('users/delete'); // && $model->status < $model::STATUS_SAVED;
+                        }
+                    ],
+                    'buttons' => [
+                        'update' => function ($url, $model) {
+                            return Html::a('<span class="fa fa-pencil-alt"></span>', $url, [
+                                'title' => Yii::t('app', 'Update'),
+                                'class'=> 'update-dialog btn btn-xs btn-success mr1',
+                                'data-form-id' => $model->id,
+                            ]);
+                        },
+                        'view' => function ($url, $model) {
+                            return Html::a('<span class="fa fa-eye"></span>', $url, [
+                                'title' => Yii::t('app', 'View'),
+                                'class'=> 'btn btn-xs btn-primary view-dialog mr1',
+                                'data-form-id' => $model->id,
+                            ]);
+                        },
+                        'delete' => function ($url, $model) {
+                            return Html::a('<span class="fa fa-trash-alt"></span>', $url, [
+                                'title' => Yii::t('app', 'Delete'),
+                                'class' => 'btn btn-xs btn-danger delete-dialog',
+                                'data-form-id' => $model->id,
+                            ]);
+                        },
+
+                ],
+            ],
+        ],
+    ]); ?>
+
+        <?php Pjax::end(); ?>
+    </div>
+</div>
+<?=  \app\widgets\ModalWindow\ModalWindow::widget([
+    'model' => 'users',
+    'crud_name' => 'users',
+    'modal_id' => 'users-modal',
+    'modal_header' => '<h3>'. Yii::t('app', 'Users') . '</h3>',
+    'active_from_class' => 'customAjaxForm',
+    'update_button' => 'update-dialog',
+    'create_button' => 'create-dialog',
+    'view_button' => 'view-dialog',
+    'delete_button' => 'delete-dialog',
+    'modal_size' => 'modal-md',
+    'grid_ajax' => 'users_pjax',
+    'confirm_message' => Yii::t('app', 'Haqiqatdan ham o\'chirmoqchimisiz?')
+]); ?>
