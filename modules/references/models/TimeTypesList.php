6a6bc0e4956152ba64b76dc86a55f51a870dd98e
muhammadsodiq <muhammadsodiq1399@gmail.com>
2022-01-28 11:13:20 +0500

    ajax crud some change

---
+++ modules/references/models/TimeTypesList.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace app\modules\references\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "time_types_list".
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $code
+ * @property int $status_id
+ * @property int $created_at
+ * @property int $created_by
+ * @property int $updated_at
+ * @property int $updated_by
+ *
+ * @property ProductLifecycle[] $productLifecycles
+ */
+class TimeTypesList extends BaseModel
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'time_types_list';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['code', 'status_id', 'created_at', 'created_by', 'updated_at', 'updated_by'], 'default', 'value' => null],
+            [['code', 'status_id', 'created_at', 'created_by', 'updated_at', 'updated_by'], 'integer'],
+            [['name'], 'string', 'max' => 50],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'name' => Yii::t('app', 'Name'),
+            'code' => Yii::t('app', 'Code'),
+            'status_id' => Yii::t('app', 'Status ID'),
+            'created_at' => Yii::t('app', 'Created At'),
+            'created_by' => Yii::t('app', 'Created By'),
+            'updated_at' => Yii::t('app', 'Updated At'),
+            'updated_by' => Yii::t('app', 'Updated By'),
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getProductLifecycles()
+    {
+        return $this->hasMany(ProductLifecycle::className(), [time_type_id => id]);
+    }
+}
