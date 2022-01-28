fcbdbca9221a1b5d22e179fffcc546d172d56d40
muhammadsodiq <muhammadsodiq1399@gmail.com>
2022-01-28 10:42:18 +0500

    ajax crud some change

--- views/layouts/plm.php
+++ views/layouts/plm.php
@@ -318,20 +318,20 @@ AppAsset::register($this);
                                     'template' => '<a href="{url}" class="{linkClass}"><i class="fa fa-pager nav-icon"></i><p>{label}</p></a>',
                                 ],
                             ],
-                            [
-                                'label' => Yii::t('app', 'References'),
-                                'url' => ['#'],
-                                'options' => ['class' => 'nav-item'],
-                                'template' => '<a href="{url}" class="{linkClass}"><i class="nav-icon fas fa-users"></i><p>{label}<i class="fas fa-angle-left right"></i></p></a>',
-                                'visible' => true,
-                                'items' => [
-                                    [
-                                        'label' => Yii::t('app', 'Shifts'),
-                                        'url' => '/references/shifts/index',
-                                        'options' => ['class' => 'nav-item'],
-                                        'active' => $controller == 'shifts' && $action  == 'index',
-                                        'template' => '<a href="{url}" class="{linkClass}"><i class="fa fa-pager nav-icon"></i><p>{label}</p></a>',
-                                    ],
+                        ],
+                        [
+                            'label' => Yii::t('app', 'References'),
+                            'url' => ['#'],
+                            'options' => ['class' => 'nav-item'],
+                            'template' => '<a href="{url}" class="{linkClass}"><i class="nav-icon fas fa-users"></i><p>{label}<i class="fas fa-angle-left right"></i></p></a>',
+                            'visible' => true,
+                            'items' => [
+                                [
+                                    'label' => Yii::t('app', 'Shifts'),
+                                    'url' => '/references/shifts/index',
+                                    'options' => ['class' => 'nav-item'],
+                                    'active' => $controller == 'shifts' && $action  == 'index',
+                                    'template' => '<a href="{url}" class="{linkClass}"><i class="fa fa-pager nav-icon"></i><p>{label}</p></a>',
                                 ],
                             ],
                         ],
