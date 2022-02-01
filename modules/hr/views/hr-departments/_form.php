825296cd6192b32df60374a30b767395bfa9de5e
fayzulloyakubov <developeryakubov@gmail.com>
2022-02-01 19:39:39 +0500

    hr_departments form

--- modules/hr/views/hr-departments/_form.php
+++ modules/hr/views/hr-departments/_form.php
@@ -12,7 +12,7 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(['options' => ['data-pjax' => true, 'class'=> 'customAjaxForm']]); ?>
 
-    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>s
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'name_ru')->textInput(['maxlength' => true]) ?>
 
