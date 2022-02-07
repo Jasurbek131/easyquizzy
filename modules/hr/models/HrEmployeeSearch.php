db0b42cf2ffa78a3333178eaafd79d5438ddf6e2
jasurbekrasuljonovrj1225@gmail.com <jasurbekrasuljonovrj1225@gmail.com>
2022-02-07 21:39:44 +0500

    hr employee search to'g'grilandi

--- modules/hr/models/HrEmployeeSearch.php
+++ modules/hr/models/HrEmployeeSearch.php
@@ -2,9 +2,9 @@
 
 namespace app\modules\hr\models;
 
+use app\widgets\Language;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
-use app\modules\hr\models\HrEmployee;
 
 /**
  * HrEmployeeSearch represents the model behind the search form of `app\modules\hr\models\HrEmployee`.
@@ -32,16 +32,17 @@ class HrEmployeeSearch extends HrEmployee
 
     /**
      * Creates data provider instance with search query applied
-     *
      * @param array $params
-     *
      * @return ActiveDataProvider
+     * @throws \Exception
      */
     public function search($params)
     {
         $query = HrEmployee::find()
             ->alias('he')
-            ->orderBy(['he.id' => SORT_DESC]);
+            ->orderBy(['he.id' => SORT_DESC])
+            ->joinWith(["hrEmployeeActivePosition.hrDepartments as hd"])
+            ->joinWith(["hrEmployeeActivePosition.hrPositions as hp"]);
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
@@ -55,9 +56,10 @@ class HrEmployeeSearch extends HrEmployee
         $query->andFilterWhere([
             'he.status_id' => $this->status_id,
         ]);
-
         $query->andFilterWhere(['ilike', 'he.firstname', $this->firstname])
             ->andFilterWhere(['ilike', 'he.lastname', $this->lastname])
+            ->andFilterWhere(['ilike', 'hd.name', $this->hr_department_id])
+            ->andFilterWhere(['ilike', sprintf('hp.%s', Language::widget()), $this->hr_position_id])
             ->andFilterWhere(['ilike', 'he.fathername', $this->fathername])
             ->andFilterWhere(['ilike', 'he.phone_number', $this->phone_number])
             ->andFilterWhere(['ilike', 'he.email', $this->email]);
