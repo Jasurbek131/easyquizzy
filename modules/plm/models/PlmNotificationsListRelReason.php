89007d39ba0b300d95bdafec85c3f366223e454c
fayzulloyakubov <developeryakubov@gmail.com>
2022-02-25 17:42:03 +0500

    tasdiqlash oynasida tasdiqlash va rad etish ishlarini amalga oshirish

--- modules/plm/models/PlmNotificationsListRelReason.php
+++ modules/plm/models/PlmNotificationsListRelReason.php
@@ -2,32 +2,32 @@
 
 namespace app\modules\plm\models;
 
-use app\modules\references\models\Defects;
+use app\modules\references\models\Reasons;
 use Yii;
 
 /**
- * This is the model class for table "plm_notification_rel_defect".
+ * This is the model class for table "plm_notifications_list_rel_reason".
  *
  * @property int $id
  * @property int $plm_notification_list_id
- * @property int $defect_id
+ * @property int $reason_id
  * @property int $status_id
  * @property int $created_by
  * @property int $created_at
  * @property int $updated_by
  * @property int $updated_at
  *
- * @property Defects $defects
  * @property PlmNotificationsList $plmNotificationsList
+ * @property Reasons $reasons
  */
-class PlmNotificationRelDefect extends BaseModel
+class PlmNotificationsListRelReason extends BaseModel
 {
     /**
      * {@inheritdoc}
      */
     public static function tableName()
     {
-        return 'plm_notification_rel_defect';
+        return 'plm_notifications_list_rel_reason';
     }
 
     /**
@@ -36,10 +36,10 @@ class PlmNotificationRelDefect extends BaseModel
     public function rules()
     {
         return [
-            [['plm_notification_list_id', 'defect_id', 'status_id', 'created_by', 'created_at', 'updated_by', 'updated_at'], 'default', 'value' => null],
-            [['plm_notification_list_id', 'defect_id', 'status_id', 'created_by', 'created_at', 'updated_by', 'updated_at'], 'integer'],
-            [['defect_id'], 'exist', 'skipOnError' => true, 'targetClass' => Defects::className(), 'targetAttribute' => ['defect_id' => 'id']],
+            [['plm_notification_list_id', 'reason_id', 'status_id', 'created_by', 'created_at', 'updated_by', 'updated_at'], 'default', 'value' => null],
+            [['plm_notification_list_id', 'reason_id', 'status_id', 'created_by', 'created_at', 'updated_by', 'updated_at'], 'integer'],
             [['plm_notification_list_id'], 'exist', 'skipOnError' => true, 'targetClass' => PlmNotificationsList::className(), 'targetAttribute' => ['plm_notification_list_id' => 'id']],
+            [['reason_id'], 'exist', 'skipOnError' => true, 'targetClass' => Reasons::className(), 'targetAttribute' => ['reason_id' => 'id']],
         ];
     }
 
@@ -51,7 +51,7 @@ class PlmNotificationRelDefect extends BaseModel
         return [
             'id' => Yii::t('app', 'ID'),
             'plm_notification_list_id' => Yii::t('app', 'Plm Notification List ID'),
-            'defect_id' => Yii::t('app', 'Defect ID'),
+            'reason_id' => Yii::t('app', 'Reason ID'),
             'status_id' => Yii::t('app', 'Status ID'),
             'created_by' => Yii::t('app', 'Created By'),
             'created_at' => Yii::t('app', 'Created At'),
@@ -63,25 +63,25 @@ class PlmNotificationRelDefect extends BaseModel
     /**
      * @return \yii\db\ActiveQuery
      */
-    public function getDefects()
+    public function getPlmNotificationsList()
     {
-        return $this->hasOne(Defects::className(), ['id' => 'defect_id']);
+        return $this->hasOne(PlmNotificationsList::className(), ['id' => 'plm_notification_list_id']);
     }
 
     /**
      * @return \yii\db\ActiveQuery
      */
-    public function getPlmNotificationsList()
+    public function getReasons()
     {
-        return $this->hasOne(PlmNotificationsList::className(), ['id' => 'plm_notification_list_id']);
+        return $this->hasOne(Reasons::className(), ['id' => 'reason_id']);
     }
-    public static function getDefectList($id = null){
+    public static function getReasonList($id = null){
         if(!empty($id)){
             $query = self::find()
-                ->alias('pnrd')
-                ->select(['d.name_uz AS defect_name','pnrd.defect_count'])
-                ->leftJoin(['d' => 'defects'],'pnrd.defect_id = d.id')
-                ->where(['pnrd.plm_notification_list_id' => $id,'pnrd.status_id' => BaseModel::STATUS_ACTIVE])
+                ->alias('pnrr')
+                ->select(['r.name_uz AS reason_name'])
+                ->leftJoin(['r' => 'reasons'],'pnrr.reason_id = r.id')
+                ->where(['pnrr.plm_notification_list_id' => $id,'pnrr.status_id' => BaseModel::STATUS_ACTIVE])
                 ->asArray()
                 ->all();
             if(!empty($query)){
--- modules/plm/views/plm-notifications-list/view.php
+++ modules/plm/views/plm-notifications-list/view.php
@@ -4,11 +4,11 @@ use app\modules\plm\models\BaseModel;
 use app\modules\references\models\Reasons;
 use yii\helpers\Html;
 use yii\helpers\Url;
-use yii\widgets\DetailView;
-use app\components\PermissionHelper as P;
 
 /* @var $this yii\web\View */
 /* @var $model app\modules\plm\models\PlmNotificationsList */
+/* @var $plmNotRelReasons app\modules\plm\models\PlmNotificationsListRelReason */
+/* @var $plmNotRelDefects app\modules\plm\models\PlmNotificationRelDefect */
 
 //$this->title = $model->id;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Plm Notifications Lists'), 'url' => ['index']];
@@ -40,14 +40,16 @@ $this->params['breadcrumbs'][] = $this->title;
                     <table class="table table-bordered">
                         <thead class="text-bold">
                             <tr>
-                                <td>№</td>
-                                <td><?=Yii::t("app","Hr Department");?></td>
-                                <td><?=Yii::t("app","Hujjat sanasi");?></td>
-                                <td><?=Yii::t("app","Smenasi");?></td>
-                                <td><?=Yii::t("app","Ish vaqti boshlanishi");?></td>
-                                <td><?=Yii::t("app","Ish vaqti tugashi");?></td>
-                                <td><?=Yii::t("app","Tamirlangan");?></td>
-                                <td><?=Yii::t("app","Yaroqsiz");?></td>
+                                <th>№</th>
+                                <th><?=Yii::t("app","Hr Department");?></th>
+                                <th><?=Yii::t("app","Document Date");?></th>
+                                <th><?=Yii::t("app","Shifts");?></th>
+                                <th><?=Yii::t("app","Equipments");?></th>
+                                <?php if(($model['token'] != 'REPAIRED') && ($model['token'] != 'SCRAPPED')):?>
+                                    <th><?=Yii::t("app","Begin Time");?></th>
+                                    <th><?=Yii::t("app","End Time");?></th>
+                                <?php endif;?>
+                                <th><?=Yii::t("app","Izoh");?></th>
                             </tr>
                         </thead>
                         <tbody>
@@ -56,14 +58,76 @@ $this->params['breadcrumbs'][] = $this->title;
                                 <td><?=$model['department'];?></td>
                                 <td><?=date('d.m.Y',strtotime($model['reg_date']));?></td>
                                 <td><?=$model['shift'];?></td>
-                                <td><?=date('d.m.Y',strtotime($model['begin_time']));?></td>
-                                <td><?=date('d.m.Y',strtotime($model['end_time']));?></td>
-                                <td><?=$model['defect'];?></td>
-                                <td><?=$model['defect_count'];?></td>
+                                <td><?=$model['equipment'];?></td>
+                                <?php if(($model['token'] != 'REPAIRED') && ($model['token'] != 'SCRAPPED')):?>
+                                <td><?=date('d.m.Y H:i',strtotime($model['begin_time']));?></td>
+                                <td><?=date('d.m.Y H:i',strtotime($model['end_time']));?></td>
+                                <?php endif;?>
+                                <td><?=$model['add_info'];?></td>
                             </tr>
                         </tbody>
                     </table>
                 </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <?php if($plmNotRelReasons):?>
+                        <table class="table table-bordered" style="background: #b6ffce;">
+                            <thead>
+                                <tr>
+                                    <th colspan="2" class="text-center"><?=Yii::t("app","Tasdiqlangan to'xtalishlar ro'yhati");?></th>
+                                </tr>
+                                <tr>
+                                    <th>№</th>
+                                    <th><?=Yii::t("app","To'xtalish nomi");?></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php $tr = 1; foreach ($plmNotRelReasons as $reason):?>
+                                    <tr>
+                                        <td class="text-center"><?=$tr;?></td>
+                                        <td><?=$reason['reason_name'];?></td>
+                                    </tr>
+                                <?php $tr++; endforeach;?>
+                            </tbody>
+                        </table>
+                    <?php else:?>
+                    <?php endif;?>
+                </div>
+                <div class="col-md-12" style="float: right">
+                    <?php if($plmNotRelDefects):
+                        $title = ""; $head = "";
+                        switch ($model['token']){
+                            case 'REPAIRED': $title = "Repaired"; $head = "Repaired Title"; break;
+                            case 'SCRAPPED': $title = "Scrapped"; $head = "Scrapped Title"; break;
+                        }
+
+                        ?>
+                        <table class="table table-bordered" style="background: #b6ffce;">
+                            <thead class="form-group">
+                            <tr>
+                                <th colspan="3" class="text-center"><?=Yii::t("app",$head);?></th>
+                            </tr>
+                            <tr>
+                                <th class="text-center">№</th>
+                                <th><?=Yii::t("app",$title);?></th>
+                                <th class="text-center"><?=Yii::t("app","Count");?></th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php $tr = 1; foreach ($plmNotRelDefects as $defect):?>
+                                <tr>
+                                    <td class="text-center"><?=$tr;?></td>
+                                    <td><?=$defect['defect_name'];?></td>
+                                    <td class="text-center"><?=$defect['defect_count'];?></td>
+                                </tr>
+                                <?php $tr++; endforeach;?>
+                            </tbody>
+                        </table>
+                    <?php else:?>
+                    <?php endif;?>
+                </div>
+
             </div>
             <div class="modal md"  id="modal_rejected" tabindex="-1" role="dialog" data-backdrop="static" data-keyboard="true">
                 <div class="modal-dialog modal-md">
@@ -91,7 +155,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 <div class="modal-dialog modal-lg">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="text-center"><?php echo Yii::t('app','Tasdiqlanadigan to\'xtalishlar ro\'yhati!')?></h5>
+                            <legend style="text-align: center"><?=Yii::t("app","List of approved stops");?></legend>
                             <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                         </div>
                         <div class="modal-body">
@@ -100,19 +164,31 @@ $this->params['breadcrumbs'][] = $this->title;
                                 ?>
                             <form action="" id="reasons-form" name="Reasons" method="POST">
                                 <input type="hidden" id="plm_notification_list_id" value="<?=$model['id'];?>">
-                                <div class="row">
-                                    <?php if(!empty($reasons)):?>
-                                        <?php foreach ($reasons as $key => $reason):?>
-                                            <div class="col-md-3">
-                                                <label for=""><?=$reason['name'];?></label>
-                                                <input type="checkbox"  name="Reasons[<?=$key?>]" value="<?=$reason['id'];?>" data-id="<?=$reason['id']?>">
-                                            </div>
-                                        <?php endforeach;?>
-                                    <?php else:?>
-                                        <h5 class="text-center"><?=Yii::t("app","Sabablar mavjud emas! Tasdiqlaysizmi ?");?></h5>
-                                        <input type="hidden" id="empty_reason" value="1">
-                                    <?php endif;?>
-                                </div>
+                                <fieldset>
+                                    <div class="container">
+                                        <div class="row checkbox_container">
+                                            <?php if(!empty($reasons)):?>
+                                                <?php foreach ($reasons as $key => $reason) : ?>
+                                                    <div class="col-lg-6">
+                                                        <div class="form-group small">
+                                                            <label class="checkbox-transform">
+                                                                <input type="checkbox" value="<?=$reason['id']?>"
+                                                                       name="Reasons[<?php echo $key ?>]"
+                                                                    <?php echo $model["name"][$key] !== null ? "checked" : "" ?>
+                                                                       class="checkbox">
+                                                                <span class="checkmark"></span>
+                                                                <span class="p-10"><?php echo $reason['name'] ?></span>
+                                                            </label>
+                                                        </div>
+                                                    </div>
+                                                <?php endforeach; ?>
+                                            <?php else:?>
+                                                <input type="hidden" id="empty_reason" value="0">
+                                                <p class="form-group text-center" style="color: #0841cb"><?php echo Yii::t("app","Tasdiqlash uchun to'xtalishlar ro'yhati mavjud emas, Tasdiqlaysizmi ?");?></p>
+                                            <?php endif;?>
+                                        </div>
+                                    </div>
+                                </fieldset>
                             </form>
                                 <br>
                                 <div class="col-md-12">
@@ -126,6 +202,32 @@ $this->params['breadcrumbs'][] = $this->title;
         </div>
     </div>
 </div>
+    <style>
+
+        fieldset .form-group {
+            margin-bottom: 0.2rem !important;
+        }
+
+        fieldset {
+            margin: 0 0 30px 0;
+            border: 2px solid #ccc;
+            border-radius: 2px;
+        }
+
+        legend {
+            background: #eee;
+            padding: 4px 10px;
+            color: #000;
+            max-width: 100% !important;
+            margin: 0 auto;
+            display: inline;
+        }
+
+        .p-10 {
+            padding-left: 15px !important;
+        }
+    </style>
+
 <?php
 $this->registerCss("
     .save_rejected{
@@ -133,6 +235,7 @@ $this->registerCss("
         margin:10px 20px;
     }
 ");
+$this->registerCssFile('/web/css/custom_checkbox.css');
 
 $url_rejected = Url::to(['ajax-rejected']);
 $url_accepted = Url::to(['ajax-accepted']);
@@ -147,13 +250,13 @@ $js = <<< JS
        });
    $('body').delegate('.save_accepted','click',function() {
       let list_id = $('#plm_notification_list_id').val();
-      let empty_reason = $('#empty-reason').val();
+      let empty_reason = $('#empty_reason').val();
       let form = $('#reasons-form');
       $.ajax({
             url: urlAccepted,
             type:"POST",
             data:{
-                list_id:list_id,
+                plm_notification_list_id:list_id,
                 empty_reason:empty_reason,
                 form:form.serializeArray(),
             },
